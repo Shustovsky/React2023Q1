1863998614d4bb846a115396b0080e13bfafef87
A. Shustovsky <a.shustovsky@gmail.com>
2023-03-13 13:03:11 +0400

    refactor: refactor from eslint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import './App.scss'
+import { useState } from 'react';
+import './App.scss';
 
 export function App() {
-
   return (
     <div className="App">
-     <h1>Hello React!</h1>
+      <h1>Hello React!</h1>
     </div>
-  )
+  );
 }
