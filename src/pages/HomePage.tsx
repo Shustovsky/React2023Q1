426fe25f3bd9128a72b97051d0f1330f66e92b0b
A. Shustovsky <a.shustovsky@gmail.com>
2023-03-15 14:45:50 +0400

    feat: Add header which shows the current page and Add React-Router version 6

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,33 @@
-export function HomePage() {
-  return <main> Home</main>;
+import { Search } from '../components/Search';
+import { Character } from '../components/Character';
+import { CharactersState, IResponse } from '../models';
+import axios from 'axios';
+import { Component } from 'react';
+
+export class HomePage extends Component<Record<string, unknown>, CharactersState> {
+  constructor(props: Record<string, unknown>) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get<IResponse>('https://rickandmortyapi.com/api/character');
+    this.setState({ characters: response.data.results });
+  }
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <main className="main-home">
+        <Search></Search>
+        <section className="products">
+          {characters.map((character) => (
+            <Character character={character} key={character.id} />
+          ))}
+        </section>
+      </main>
+    );
+  }
 }
