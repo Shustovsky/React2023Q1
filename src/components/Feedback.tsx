b2b0d16c6f1ef3634072bcb49a9957dd1204c7a6
A. Shustovsky <a.shustovsky@gmail.com>
2023-03-28 12:42:57 +0400

    fix: fix problems with picture

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,23 +1,16 @@
 import { FeedbackProps } from '../models';
-import { useState, useEffect } from 'react';
 import './feedback.scss';
 
 export function Feedback({ feedback }: FeedbackProps): JSX.Element {
-  const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    if (feedback.profilePicture) {
-      const fileReader = new FileReader();
-      fileReader.onload = () => {
-        setImageUrl(fileReader.result as string);
-      };
-      fileReader.readAsDataURL(feedback.profilePicture[0]);
-    }
-  }, [feedback.profilePicture]);
-
   return (
     <div className="feedback">
-      {imageUrl && <img className="feedback_img" src={imageUrl} alt="Profile" />}
+      {feedback.profilePicture[0] && (
+        <img
+          className="feedback_img"
+          src={URL.createObjectURL(feedback.profilePicture[0])}
+          alt="Profile"
+        />
+      )}
       <div className="feedback_name">Name: {feedback.name}</div>
       <div className="feedback_rate">
         Rate: <span className="feedback_rate_bold">{feedback.rate}</span>
