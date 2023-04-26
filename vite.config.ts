b303aca7704d4fe43b4ebb76cea53bb8411ced97
A. Shustovsky <a.shustovsky@gmail.com>
2023-04-26 12:59:02 +0400

    feat: setting up cypress

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,11 @@
 /// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['istanbul']],
-      },
-    }),
-  ],
+  plugins: [react(), istanbul({ cypress: true, requireEnv: false })],
   test: {
     globals: true,
     environment: 'jsdom',
